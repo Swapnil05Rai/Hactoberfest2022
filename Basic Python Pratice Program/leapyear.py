75932923903e8aba361fa98b00722959b46b83c1
Swapnil05Rai <87640978+Swapnil05Rai@users.noreply.github.com>
2022-10-03 12:23:22 +0530

    Update leapyear.py

--- Basic Python Pratice Program/leapyear.py
+++ Basic Python Pratice Program/leapyear.py
@@ -1,5 +1,5 @@
 y=int(input())
 if (y%100==0 and y%400==0) or y%4==0: 
-    print("leap year")
+    print("The entered year is a leap year")
 else: 
-    print("not a leap year")
+    print("The entered year is not a leap year")
